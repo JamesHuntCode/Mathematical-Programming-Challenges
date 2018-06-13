67c53b814985853dbc61eb1872967cc078331818
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-13 16:17:34 +0100

    sum of multiples of x and y - initial commit - files created

---
+++ Sum-of-Multiples/SumOfMultiples/src/sumofmultiples/SumOfMultiples.java
@@ -0,0 +1,10 @@
+package sumofmultiples;
+
+/** program to find all multiples of 3 or 5 below 1000 then find the result of adding them all together. **/
+public class SumOfMultiples {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
