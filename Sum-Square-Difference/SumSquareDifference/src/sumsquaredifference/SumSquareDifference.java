4621876bbdda0590188f3d01500da61d26c23016
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-14 23:42:40 +0100

    all methods created - testing in progress

--- Sum-Square-Difference/SumSquareDifference/src/sumsquaredifference/SumSquareDifference.java
+++ Sum-Square-Difference/SumSquareDifference/src/sumsquaredifference/SumSquareDifference.java
@@ -2,8 +2,56 @@ package sumsquaredifference;
 
 public class SumSquareDifference {
 
+    /** program to take the sum of a sequence where each element is squared, then square the entire sequence and find the difference. **/
     public static void main(String[] args) {
         
+        // Create sequence.
+        int[] mySequence = createSequence(0, 10);
+    }
+    
+    /** method to create a linear sequence of integers. **/
+    public static int[] createSequence(int start, int end)
+    {
+        int[] sequence = new int[end];
+        
+        for (int i = start; i < end; i++)
+        {
+            sequence[i] = (i * i);
+        }
+            
+        return sequence;
+    }
+    
+    /** method to iterate over a sequence, square each element and return the sum of all elements. **/
+    public static long getSquareSum(int[] sequence)
+    {
+        long sum = 0;
+        
+        for (int i = 0; i < sequence.length; i++)
+        {
+            sum += (sequence[i] * sequence[i]);
+        }
+        
+        return sum;
+    }
+    
+    /* method to take an array and square the sum of all elements. **/
+    public static long squareElementSum(int[] elements)
+    {
+        long elementSum = 0;
+        
+        for (int i = 0; i < elements.length; i++)
+        {
+            elementSum += elements[i];
+        }
+        
+        return (elementSum * elementSum);
+    }
+    
+    /** method to return the difference between two numerical values **/
+    public static long getDifference(long largerValue, long smallerValue)
+    {
+        return (largerValue - smallerValue);
     }
     
 }
