8df701cf47591905fce79200637679a4c0104929
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-15 00:02:07 +0100

    numerical expression optimiser - files created - initial commit

---
+++ Roman-Numeral-Optimization/RomanNumeralOptimization/src/romannumeraloptimization/RomanNumeralOptimization.java
@@ -0,0 +1,13 @@
+package romannumeraloptimization;
+
+/** program to optimise the number of Roman numerals used to express numerical values. **/
+/* for example... 'IIIII' is valid, however 'V' in this situation is better. **/
+public class RomanNumeralOptimization {
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
