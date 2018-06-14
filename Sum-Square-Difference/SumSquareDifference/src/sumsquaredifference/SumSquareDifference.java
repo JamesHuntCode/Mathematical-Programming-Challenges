5565e693f9a545ac32738fcb7659ffd1cb3a916d
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-14 23:54:47 +0100

    program complete - tested and creates desired results

--- Sum-Square-Difference/SumSquareDifference/src/sumsquaredifference/SumSquareDifference.java
+++ Sum-Square-Difference/SumSquareDifference/src/sumsquaredifference/SumSquareDifference.java
@@ -7,16 +7,29 @@ public class SumSquareDifference {
         
         // Create sequence.
         int[] mySequence = createSequence(0, 10);
+        
+        // Square all elements and get the total.
+        long squaredElementSum = getSquareSum(mySequence);
+        System.out.print("The sum of all elements squared = " + squaredElementSum + ".\n\n");
+        
+        // Square the entire sequence.
+        long squaredSequence = squareSequenceSum(mySequence);
+        System.out.print("The sum the entire sequence squared = " + squaredSequence + ".\n\n");
+        
+        // Get difference.
+        long difference = getDifference(squaredSequence, squaredElementSum);
+        System.out.print("The difference between the two numbers = " + difference + ".\n\n");
+        
     }
     
     /** method to create a linear sequence of integers. **/
     public static int[] createSequence(int start, int end)
     {
-        int[] sequence = new int[end];
+        int[] sequence = new int[(end + 1)];
         
-        for (int i = start; i < end; i++)
+        for (int i = start; i < (end + 1); i++)
         {
-            sequence[i] = (i * i);
+            sequence[i] = i;
         }
             
         return sequence;
@@ -36,13 +49,13 @@ public class SumSquareDifference {
     }
     
     /* method to take an array and square the sum of all elements. **/
-    public static long squareElementSum(int[] elements)
+    public static long squareSequenceSum(int[] sequence)
     {
         long elementSum = 0;
         
-        for (int i = 0; i < elements.length; i++)
+        for (int i = 0; i < sequence.length; i++)
         {
-            elementSum += elements[i];
+            elementSum += sequence[i];
         }
         
         return (elementSum * elementSum);
