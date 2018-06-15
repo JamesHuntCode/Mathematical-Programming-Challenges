63659111d95d25edd0c9a84a6aa06d664a1692a4
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-15 21:08:23 +0100

    created some demo inputs, methods and workflow laid out

--- Roman-Numeral-Optimization/RomanNumeralOptimization/src/romannumeraloptimization/RomanNumeralOptimization.java
+++ Roman-Numeral-Optimization/RomanNumeralOptimization/src/romannumeraloptimization/RomanNumeralOptimization.java
@@ -6,8 +6,32 @@ public class RomanNumeralOptimization {
     
     public static void main(String[] args) {
         
+        // Create some demo strings that need to be optimised.  
+        String demoInput1 = "IIIII"; // 5.
+        String demoInput2 = "IIIIII"; // 6.
+        String demoInput3 = "VIIIII"; // 10.
+        String demoInput4 = "IIIIIV"; // 10.
+        String demoInput5 = "IIIIIVVV"; // 20.
+        String demoInput6= "VVVV"; // 20.
+        
+        // Optimise all inputs and output new answers.
+        System.out.print(optimise(demoInput1) + "\n\n");
+        System.out.print(optimise(demoInput2) + "\n\n");
+        System.out.print(optimise(demoInput3) + "\n\n");
+        System.out.print(optimise(demoInput4) + "\n\n");
+        System.out.print(optimise(demoInput5) + "\n\n");
+        System.out.print(optimise(demoInput6) + "\n\n");
+        
+    }
+    
+    /** method to minimise the number of characters needed to represent a Roman numeral string. **/
+    public static String optimise(String input)
+    {
+        String formattedOutput = "";
         
         
+        
+        return formattedOutput;
     }
     
 }
