f11e50acdc0a77b6ec1f20125f2db1cb588f8f2a
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-15 22:31:09 +0100

    optimisation of I characters done, now repeating for the rest

--- Roman-Numeral-Optimization/RomanNumeralOptimization/src/romannumeraloptimization/RomanNumeralOptimization.java
+++ Roman-Numeral-Optimization/RomanNumeralOptimization/src/romannumeraloptimization/RomanNumeralOptimization.java
@@ -16,6 +16,8 @@ public class RomanNumeralOptimization {
         String demoInput5 = "IIIIIVVV"; // 20.
         String demoInput6= "VVVV"; // 20.
         
+        System.out.print("Primary Algorithm: \n\n");
+        
         // Optimise all inputs and output new answers.
         System.out.print(optimise(demoInput1) + "\n\n");
         System.out.print(optimise(demoInput2) + "\n\n");
@@ -23,6 +25,16 @@ public class RomanNumeralOptimization {
         System.out.print(optimise(demoInput4) + "\n\n");
         System.out.print(optimise(demoInput5) + "\n\n");
         System.out.print(optimise(demoInput6) + "\n\n");
+        
+        System.out.print("Alternate algorithm: \n\n");
+        
+        // Optimise all inputs and output new answers.
+        System.out.print(optimiseAltVersion(demoInput1) + "\n\n");
+        System.out.print(optimiseAltVersion(demoInput2) + "\n\n");
+        System.out.print(optimiseAltVersion(demoInput3) + "\n\n");
+        System.out.print(optimiseAltVersion(demoInput4) + "\n\n");
+        System.out.print(optimiseAltVersion(demoInput5) + "\n\n");
+        System.out.print(optimiseAltVersion(demoInput6) + "\n\n");
         
     }
     
@@ -32,13 +44,48 @@ public class RomanNumeralOptimization {
     {
         String formattedOutput = "";
         
+        int Icounter = 0;
+        
         // Optimise the amount of 'I' characters.
         for (int i = 0; i < input.length(); i++)
         {
+            char currentChar = input.charAt(i);
+            
+            if (currentChar == 'I')
+            {
+                Icounter++;
+            }
+            else 
+            {
+                formattedOutput += currentChar;
+            }
+            
+            if (Icounter == 5)
+            {
+                Icounter = 0;
+                formattedOutput += "V";
+            }
+            else if ((Icounter != 5) && (i == input.length() - 1))
+            {
+                for (int j = 0; j < Icounter; j++)
+                {
+                    formattedOutput += "I";
+                }
+            }
             
         }
         
+        int Vcounter = 0;
+        
         // Optimise the amount of 'V' characters.
+        for (int i = 0; i < input.length(); i++)
+        {
+            char currentChar = input.charAt(i);
+            
+            
+        }
+        
+        /** shift all remaining 'I' characters to the end of the formatted string. **/
         for (int i = 0; i < input.length(); i++)
         {
             
