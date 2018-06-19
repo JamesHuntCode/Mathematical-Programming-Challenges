943f6369b3dc04a3e2d404c15bcbda67714e9cec
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-19 21:33:17 +0100

    files created - initial commit for largest product in a matrix

---
+++ Largest-Product-In-Grid/LargestProductInGrid/src/largestproductingrid/LargestProductInGrid.java
@@ -0,0 +1,11 @@
+package largestproductingrid;
+
+public class LargestProductInGrid {
+    
+    /** product to find the largest sequence (of 'x' digits) in a grid by checking all possible directions from an origin point. **/
+    public static void main(String[] args) {
+       
+        
+    }
+    
+}
