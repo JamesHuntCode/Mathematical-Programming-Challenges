c37e753640c79133b57597fb6ece3ff3d76ae8d4
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-21 23:17:38 +0100

    functions written out - workflow planned for challenge

--- Pythagorean-Triplet/SpecialPythagoreanTriplet/src/specialpythagoreantriplet/SpecialPythagoreanTriplet.java
+++ Pythagorean-Triplet/SpecialPythagoreanTriplet/src/specialpythagoreantriplet/SpecialPythagoreanTriplet.java
@@ -8,8 +8,29 @@ public class SpecialPythagoreanTriplet {
 
     public static void main(String[] args) {
        
+       // Log the product of the pythagorean triplet of 1000.
+       int product = getProductOfTriplet(findPythagoreanTriplet(25));
+        
+    }
+    
+    /** method to find the values that when squared (and summed), equal the input value of the function. **/
+    public static ArrayList<Integer> findPythagoreanTriplet(int input)
+    {
+        ArrayList<Integer> values = new ArrayList<>();
         
         
+        
+        return values;
+    }
+    
+    /** method to get product of values passed in via the array-list. **/
+    public static int getProductOfTriplet(ArrayList<Integer> tripletValues)
+    {
+        int product = 0;
+        
+        
+        
+        return product;
     }
     
 }
