b3d43dc22aff702b278535ee95d0afc36d8d32f4
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-21 22:56:16 +0100

    pythagorean triplet - files created / initial commit

---
+++ Pythagorean-Triplet/SpecialPythagoreanTriplet/src/specialpythagoreantriplet/SpecialPythagoreanTriplet.java
@@ -0,0 +1,15 @@
+package specialpythagoreantriplet;
+
+import java.math.*;
+import java.util.*;
+
+/** program to determine what three numbers when squared produce a desired number. **/
+public class SpecialPythagoreanTriplet {
+
+    public static void main(String[] args) {
+       
+        
+        
+    }
+    
+}
