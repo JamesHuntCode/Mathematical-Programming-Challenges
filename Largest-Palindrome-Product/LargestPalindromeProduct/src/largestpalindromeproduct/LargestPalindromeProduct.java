d87ab626481836a154fb24a04e79633c8e751e19
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-24 20:36:56 +0100

    largest palindrome product challenge - files created - initial commit

---
+++ Largest-Palindrome-Product/LargestPalindromeProduct/src/largestpalindromeproduct/LargestPalindromeProduct.java
@@ -0,0 +1,15 @@
+package largestpalindromeproduct;
+
+import java.lang.*;
+import java.math.*;
+
+/** program to find the largest number that can be made from 'x' digits when multiplied to produce the number that reads the same, even when reversed. **/
+public class LargestPalindromeProduct {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
