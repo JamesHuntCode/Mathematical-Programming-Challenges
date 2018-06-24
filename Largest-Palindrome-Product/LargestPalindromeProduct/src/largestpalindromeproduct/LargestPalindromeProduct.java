cb0d8ec7fe6b2e794877ed5de10c3774528d9312
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-24 20:39:26 +0100

    workflow established - ready to tackle main challenge

--- Largest-Palindrome-Product/LargestPalindromeProduct/src/largestpalindromeproduct/LargestPalindromeProduct.java
+++ Largest-Palindrome-Product/LargestPalindromeProduct/src/largestpalindromeproduct/LargestPalindromeProduct.java
@@ -8,8 +8,18 @@ public class LargestPalindromeProduct {
 
     public static void main(String[] args) {
         
+        System.out.print("The largest palindrome product that can be made from 3 digits is " + getLargestPalindromeProduct(2) + ".\n\n");
+        
+    }
+    
+    /** method to calculate the largest palindrome product. **/
+    public static int getLargestPalindromeProduct(int digitLimit)
+    {
+        int largestValue = 0;
         
         
+        
+        return largestValue;
     }
     
 }
