c0901f931b3be2024e870f457a1fee4038f8713d
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-24 20:58:19 +0100

    idea for a potential algorithm implemented - testing now

--- Largest-Palindrome-Product/LargestPalindromeProduct/src/largestpalindromeproduct/LargestPalindromeProduct.java
+++ Largest-Palindrome-Product/LargestPalindromeProduct/src/largestpalindromeproduct/LargestPalindromeProduct.java
@@ -8,7 +8,7 @@ public class LargestPalindromeProduct {
 
     public static void main(String[] args) {
         
-        System.out.print("The largest palindrome product that can be made from 3 digits is " + getLargestPalindromeProduct(2) + ".\n\n");
+        System.out.print("The largest palindrome product that can be made from 3 digits is " + getLargestPalindromeProduct(99) + ".\n\n");
         
     }
     
@@ -17,7 +17,20 @@ public class LargestPalindromeProduct {
     {
         int largestValue = 0;
         
-        
+        for (int i = 2; i < digitLimit; i++)
+        {
+            for (int j = 2; j < digitLimit; j++)
+            {
+                
+                int integerValue = (i * j);
+                String stringValue = String.valueOf(integerValue);
+                
+                if (((i * j) > largestValue) && (stringValue.length() % 2 == 0) && (stringValue.substring(0, stringValue.length() / 2).equals(stringValue.substring(stringValue.length() / 2, stringValue.length()))))
+                {
+                    largestValue = (i * j);
+                }
+            }
+        }
         
         return largestValue;
     }
