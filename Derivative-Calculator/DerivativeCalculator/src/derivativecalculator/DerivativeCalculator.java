aef9ac5fc78e8bd6a7db75523666729fd73e2107
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-25 18:15:12 +0100

    derivative calculator - methods laid out

---
+++ Derivative-Calculator/DerivativeCalculator/src/derivativecalculator/DerivativeCalculator.java
@@ -0,0 +1,25 @@
+package derivativecalculator;
+
+import java.lang.*;
+import java.math.*;
+
+/** program to implement the power rule to calculate the derivative of a function **/
+public class DerivativeCalculator {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+    /** method to apply the power rule to the equation d/dx(x) **/
+    public static String getDerivativeOf(String inputFunction)
+    {
+        String derivative = "";
+        
+        
+        
+        return derivative;
+    }
+    
+}
