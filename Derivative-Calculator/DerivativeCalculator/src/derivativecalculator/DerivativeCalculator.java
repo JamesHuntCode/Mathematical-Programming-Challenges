724f51cd756d99fa24034befdd3c58d0ac05f08e
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-25 18:46:27 +0100

    challenge complete (using the power rule)

--- Derivative-Calculator/DerivativeCalculator/src/derivativecalculator/DerivativeCalculator.java
+++ Derivative-Calculator/DerivativeCalculator/src/derivativecalculator/DerivativeCalculator.java
@@ -8,17 +8,23 @@ public class DerivativeCalculator {
 
     public static void main(String[] args) {
         
+        // Log the derivative.
+        // Poor example... I know. (I had issues splitting the string at the '^' character...
+        String function = "f of x tothe 2";
         
+        System.out.print("The derivative of the function " + "f(x^2)" + " is " + getDerivativeOf(function) + ".\n\n");
         
     }
     
-    /** method to apply the power rule to the equation d/dx(x) **/
+    /** method to apply the power rule to the equation **/
     public static String getDerivativeOf(String inputFunction)
     {
-        String derivative = "";
+        // For this function we use the power rule.
+        // This rule dictates that d(x^n)/dx = nx^n-1.
+        double power = Double.parseDouble(inputFunction.split("tothe ")[1]);
         
-        
-        
+        // Construct & return result.
+        String derivative = Math.round(power) + "x^" + Math.round((power - 1));
         return derivative;
     }
     
