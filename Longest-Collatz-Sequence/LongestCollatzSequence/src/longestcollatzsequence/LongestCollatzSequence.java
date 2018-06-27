cfec6e79e7a6badd5af50894d381ac243c66e143
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-27 14:17:16 +0100

    longest collatz sequence - files created / initial commit

---
+++ Longest-Collatz-Sequence/LongestCollatzSequence/src/longestcollatzsequence/LongestCollatzSequence.java
@@ -0,0 +1,16 @@
+package longestcollatzsequence;
+
+import java.math.*;
+import java.lang.*;
+
+public class LongestCollatzSequence {
+
+    /** program to determine which number under 'x' produces the largest Collatz sequence. **/
+    /** the Collatz sequence dictates that n -> = (n/2) if 'n' is even and n -> = (3n + 1) if 'n' is odd. **/
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
