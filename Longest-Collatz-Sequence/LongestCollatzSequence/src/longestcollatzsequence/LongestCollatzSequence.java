08fa7020272c423f92c8a630be16091546860f74
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-27 14:31:57 +0100

    generating correct sequence, now to locate longest

--- Longest-Collatz-Sequence/LongestCollatzSequence/src/longestcollatzsequence/LongestCollatzSequence.java
+++ Longest-Collatz-Sequence/LongestCollatzSequence/src/longestcollatzsequence/LongestCollatzSequence.java
@@ -2,6 +2,7 @@ package longestcollatzsequence;
 
 import java.math.*;
 import java.lang.*;
+import java.util.*;
 
 public class LongestCollatzSequence {
 
@@ -9,8 +10,40 @@ public class LongestCollatzSequence {
     /** the Collatz sequence dictates that n -> = (n/2) if 'n' is even and n -> = (3n + 1) if 'n' is odd. **/
     public static void main(String[] args) {
         
+        // Log the result.
+        ArrayList<Integer> emptyList = new ArrayList<>();
+        ArrayList<Integer> CollatzSequence = calculateNextCollatz(13, emptyList);
         
+        // Output the entire sequence.
+        for (int i = 0; i < CollatzSequence.size(); i++)
+        {
+            System.out.print(CollatzSequence.get(i) + "\n");
+        }
         
     }
     
+    /** method to recursively calculate the next number in the Collatz sequence. **/
+    public static ArrayList<Integer> calculateNextCollatz(int n, ArrayList<Integer> sequence)
+    {
+        sequence.add(n);
+        
+        if (n == 1)
+        {
+            return sequence;
+        }
+        else if (n % 2 == 0)
+        {
+            return calculateNextCollatz(n / 2, sequence);
+        } 
+        else 
+        {
+            return calculateNextCollatz((3 * n) + 1, sequence);
+        }
+    }
+    
+    /** method to calculate the length of the Collatz sequence passed in. **/
+    public static int getLength(ArrayList<Integer> sequence)
+    {
+        return sequence.size();
+    }
 }
