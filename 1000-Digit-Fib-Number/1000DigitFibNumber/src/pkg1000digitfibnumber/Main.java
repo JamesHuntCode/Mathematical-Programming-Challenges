f559be292d293081a5dcec2c760f7c7872319f11
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-27 14:57:35 +0100

    1000 digit fibonacci number challenge - files created / initial commit

---
+++ 1000-Digit-Fib-Number/1000DigitFibNumber/src/pkg1000digitfibnumber/Main.java
@@ -0,0 +1,29 @@
+package pkg1000digitfibnumber;
+
+import java.util.*;
+
+public class Main {
+
+    /** program to find the index and value of the first Fibonacci number to contain 1000 digits. **/
+    public static void main(String[] args) {
+        
+        // Create sequence.
+        
+              
+        // Log the result.
+        
+        
+    }
+    
+    /** method to create the Fibonacci Sequence up until a defined limit **/
+    /** this method takes in iterative approach to the Fibonacci sequence. **/
+    public static ArrayList<Integer> createFibonacciSequence(int limit)
+    {
+        ArrayList<Integer> FibSequence = new ArrayList<>();
+        
+        
+        
+        return FibSequence;
+    }
+    
+}
