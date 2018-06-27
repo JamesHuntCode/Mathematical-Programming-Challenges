41eba0a71676b3e026e6efc9a0f561c5ebbe6ec2
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-27 22:23:36 +0100

    node structure coded

--- Largest-Sum-Pathfinder/LargestSumPathfinder/src/largestsumpathfinder/LargestSumPathfinder.java
+++ Largest-Sum-Pathfinder/LargestSumPathfinder/src/largestsumpathfinder/LargestSumPathfinder.java
@@ -2,11 +2,28 @@ package largestsumpathfinder;
 
 public class LargestSumPathfinder {
 
-    
+    /** program to traverse tree like structure whilst attempting to accumulate the largest sum of the node values visited. **/
     public static void main(String[] args) {
         
         
         
     }
     
+}
+
+/** node class from which the tree is constructed. **/
+class Node {
+    
+    /** node properties **/
+    Node left;
+    Node right;
+    int value;
+    
+    /** node constructor function **/
+    public Node(int val)
+    {
+        this.value = val;
+        this.left = null;
+        this.right = null;
+    }
 }
