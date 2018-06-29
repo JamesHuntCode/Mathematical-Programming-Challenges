3c98c97ef557a0c881c3af9550ef83ee16418833
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-29 22:12:49 +0100

    consecutive prime sum challenge - files created / initial commit

---
+++ Consecutive-Prime-Sum/ConsecutivePrimeSum/src/consecutiveprimesum/ConsecutivePrimeSum.java
@@ -0,0 +1,16 @@
+package consecutiveprimesum;
+
+import java.math.*;
+import java.util.*;
+import java.lang.*;
+
+public class ConsecutivePrimeSum {
+
+   /** program to find the largest prime number made only from other prime numbers (below a certain threshold. **/
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
