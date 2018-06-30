8c6586b245f839b854126658aadd3b1f209fc30b
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-30 16:16:10 +0100

    workflow laid out - ready to complete challenge

--- Consecutive-Prime-Sum/ConsecutivePrimeSum/src/consecutiveprimesum/ConsecutivePrimeSum.java
+++ Consecutive-Prime-Sum/ConsecutivePrimeSum/src/consecutiveprimesum/ConsecutivePrimeSum.java
@@ -9,7 +9,19 @@ public class ConsecutivePrimeSum {
    /** program to find the largest prime number made only from other prime numbers (below a certain threshold. **/
     public static void main(String[] args) {
         
+        // Locate and log the result.
+        int largestPrimeSum = 0;
         
+        for (int i = 0; i < 100; i ++)
+        {
+            for (int j = 0; j < 100; j++)
+            {
+                
+            }
+        }
+        
+        // Print result.
+        System.out.print("The largest prime made from two other primes below 100 is " + largestPrimeSum + ".\n\n");
         
     }
     
