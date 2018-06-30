5d3c07cfe2f511939973093b6bacac68f559276d
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-30 17:10:45 +0100

    highly divisible triangular number challenge - initial commit / files created

---
+++ Highly-Divisible-Triangular-Number/HighlyDivisibleTriangularNumber/src/highlydivisibletriangularnumber/HighlyDivisibleTriangularNumber.java
@@ -0,0 +1,16 @@
+package highlydivisibletriangularnumber;
+
+import java.util.*;
+import java.math.*;
+import java.lang.*;
+
+public class HighlyDivisibleTriangularNumber {
+
+    /** program to find the first triangular number with more than 'x' divisors. **/
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
