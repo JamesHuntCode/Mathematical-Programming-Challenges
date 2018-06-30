efed0c361260cbb606feb464f0c5821144c51916
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-06-30 16:34:42 +0100

    expanding the derivative calculator to support more complex functions

--- Derivative-Calculator/DerivativeCalculator/src/derivativecalculator/DerivativeCalculator.java
+++ Derivative-Calculator/DerivativeCalculator/src/derivativecalculator/DerivativeCalculator.java
@@ -12,20 +12,43 @@ public class DerivativeCalculator {
         // Poor example... I know. (I had issues splitting the string at the '^' character...
         String function = "f of x tothe 2";
         
-        System.out.print("The derivative of the function " + "f(x^2)" + " is " + getDerivativeOf(function) + ".\n\n");
+        System.out.print("The derivative of the function " + "f(x^2)" + " is " + getDerivativeOf(function, "none") + ".\n\n");
         
     }
     
-    /** method to apply the power rule to the equation **/
-    public static String getDerivativeOf(String inputFunction)
+    /** method to calculate the derivate of the function passed in (an action must be passed also, for example; addition, division etc) **/
+    public static String getDerivativeOf(String inputFunction, String action)
     {
-        // For this function we use the power rule.
-        // This rule dictates that d(x^n)/dx = nx^n-1.
-        double power = Double.parseDouble(inputFunction.split("tothe ")[1]);
+        if (action.equals("none"))
+        {
+            double power = Double.parseDouble(inputFunction.split("tothe ")[1]);
+
+            String derivative = Math.round(power) + "x^" + Math.round((power - 1));
+            return derivative;
+        }
         
-        // Construct & return result.
-        String derivative = Math.round(power) + "x^" + Math.round((power - 1));
-        return derivative;
+        else if  (action.equals("addition"))
+        {
+            String derivative = "";
+            
+            
+            
+            return derivative;
+        }
+        
+        else if (action.equals("multiplication"))
+        {
+            String derivative = "";
+            
+            
+            
+            return derivative;
+        }
+        
+        else 
+        {
+            return "Not implemented yet.";
+        }
     }
     
 }
