891a6d80143bd65888495cd43dfbaa515345eda5
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-07-03 02:51:11 +0100

    amicable pairs challenge - files created / initial commit

---
+++ Amicable-Numbers/AmicablePairs/src/amicablepairs/AmicablePairs.java
@@ -0,0 +1,26 @@
+package amicablepairs;
+
+import java.math.*;
+import java.lang.*;
+import java.util.*;
+
+public class AmicablePairs {
+
+    /** program to find amicable pairs - for example: if d(a) = d(b) = a, then a and b are an amicable pair.  **/
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+    /** method to find all the proper divisors of the input and sum them. **/
+    public static int getSumOfProperDivisors(int value)
+    {
+        int sum = 0;
+        
+        
+        
+        return sum;
+    }
+    
+}
