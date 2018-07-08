df2a9582b8081a4fe1d2d34577b775f7a989757f
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-07-08 20:25:51 +0100

    prime replacement challenge - files created / initial commit

---
+++ Prime-Digit-Replacement/PrimeDigitReplacement/src/primedigitreplacement/PrimeDigitReplacement.java
@@ -0,0 +1,25 @@
+package primedigitreplacement;
+
+import java.math.*;
+import java.util.*;
+import java.lang.*;
+
+public class PrimeDigitReplacement {
+
+    /** program to find the first prime number that when you replace 'x' amount of digits, create another 'y' amount of prime numbers. **/
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+    /** method to calculate the prime numbers up until the specified term. **/
+    public static ArrayList<Integer> getPrimes(int terms)
+    {
+        ArrayList<Integer> primes = new ArrayList<>();
+        
+        
+        
+        return primes;
+    }
+}
