761c17dea4a4d969f3add9478e68fc2ffb7bcd9d
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-07-30 21:40:29 +0100

    main methods written out needed - now working on main logic

--- Circular-Primes/CircularPrimes/src/circularprimes/CircularPrimes.java
+++ Circular-Primes/CircularPrimes/src/circularprimes/CircularPrimes.java
@@ -9,6 +9,13 @@ public class CircularPrimes {
     /** Program to find all of the numbers below a certain amount which when rearranged, all combinations are prime. **/
     public static void main(String[] args) {
        
+        int limit = 100;
+        ArrayList<Integer> primes = getPrimes(10);
+        
+        for (int i = 0; i < limit; i++)
+        {
+            
+        }
     }
     
     /* Method to return out a list of prime to an specified number of terms. **/
@@ -16,7 +23,17 @@ public class CircularPrimes {
     {
         ArrayList<Integer> primes = new ArrayList<>();
         
-        
+        for (int i = 0; i < terms; i++)
+        {
+            if (isPrime(i))
+            {
+                primes.add(i);
+            }
+            else
+            {
+                terms++;
+            }
+        }
         
         return primes;
     }
@@ -29,5 +46,16 @@ public class CircularPrimes {
         
         
         return possibleValues;
+    }
+    
+    /** method to check if a specific value is prime or not. **/
+    public static boolean isPrime(int number)
+    {
+        for (int i = 2; i < (number - 1); i++)
+        {
+            if ((number % i == 0) && (number != 1)) return false;
+        }
+        
+        return true;
     }
 }
