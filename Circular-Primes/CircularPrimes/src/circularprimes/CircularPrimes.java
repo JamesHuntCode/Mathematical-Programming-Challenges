06c82e2353038bf114d6c3083199db25f5fdf466
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-07-30 21:11:56 +0100

    methods laid out to tackle challenge

--- Circular-Primes/CircularPrimes/src/circularprimes/CircularPrimes.java
+++ Circular-Primes/CircularPrimes/src/circularprimes/CircularPrimes.java
@@ -1,21 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package circularprimes;
 
-/**
- *
- * @author James
- */
+import java.lang.*;
+import java.util.*;
+import java.math.*;
+
 public class CircularPrimes {
 
-    /**
-     * @param args the command line arguments
-     */
+    /** Program to find all of the numbers below a certain amount which when rearranged, all combinations are prime. **/
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
+    /* Method to return out a list of prime to an specified number of terms. **/
+    public static ArrayList<Integer> getPrimes(int terms)
+    {
+        ArrayList<Integer> primes = new ArrayList<>();
+        
+        
+        
+        return primes;
+    }
+    
+    /** Method to return an array of the possible combinations of numbers based on the input prime number. **/
+    public static int[] switchDigits(int primeNumber)
+    {
+        int[] possibleValues = new int[String.valueOf(primeNumber).length()];
+        
+        
+        
+        return possibleValues;
+    }
 }
