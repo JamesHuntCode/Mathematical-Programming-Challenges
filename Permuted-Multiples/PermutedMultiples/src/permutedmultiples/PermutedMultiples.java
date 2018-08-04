13220295811299d180bd7cdb1af98f25b7ee2a04
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-08-04 16:58:48 +0100

    permuted multiples challenge - files created / initial commit

---
+++ Permuted-Multiples/PermutedMultiples/src/permutedmultiples/PermutedMultiples.java
@@ -0,0 +1,14 @@
+package permutedmultiples;
+
+import java.lang.*;
+import java.math.*;
+import java.util.*;
+
+/** program to find the smallest integer where x contains the same digits as 2x, 3x, 4x, 5x & 6x. **/
+public class PermutedMultiples {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
