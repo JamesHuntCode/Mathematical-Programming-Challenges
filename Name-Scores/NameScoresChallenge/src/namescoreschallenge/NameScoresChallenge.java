8551bfcfd860aa2b6ea76cca1e9226814da56190
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-08-27 21:47:21 +0100

    name scores challenge program files created

---
+++ Name-Scores/NameScoresChallenge/src/namescoreschallenge/NameScoresChallenge.java
@@ -0,0 +1,13 @@
+package namescoreschallenge;
+
+import java.lang.*;
+import java.math.*;
+import java.util.*;
+
+public class NameScoresChallenge {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
