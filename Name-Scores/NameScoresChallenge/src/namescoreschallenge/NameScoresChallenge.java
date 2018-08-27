ac9a18c3573b4b6fcf5a8d9c2b7a9f0a6332dedd
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-08-27 21:50:30 +0100

    methods needed for challenge created

--- Name-Scores/NameScoresChallenge/src/namescoreschallenge/NameScoresChallenge.java
+++ Name-Scores/NameScoresChallenge/src/namescoreschallenge/NameScoresChallenge.java
@@ -8,6 +8,25 @@ public class NameScoresChallenge {
     
     public static void main(String[] args) {
         
+        String name = "James";
+        
+        System.out.print(name + " has a letter score of " + getScoreForName(name));
+                
+    }
+    
+    public static int getScoreForName(String name)
+    {
+        return 1;
+    }
+    
+    public static int getScoreFromLetter(String letter)
+    {
+        return 1;
+    }
+    
+    public static String getAlphabet()
+    {
+        return "abcdefghijklmnopqrstuvwxyz";
     }
     
 }
