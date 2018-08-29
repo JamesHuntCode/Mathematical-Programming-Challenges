9939c909f137acc9f04fcbadfddeee388e12853b
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-08-29 20:53:42 +0100

    methods needed for challenge planned out

--- Non-Abundant-Sums/NonAbundantSums/src/nonabundantsums/NonAbundantSums.java
+++ Non-Abundant-Sums/NonAbundantSums/src/nonabundantsums/NonAbundantSums.java
@@ -1,21 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nonabundantsums;
 
-/**
- *
- * @author James
- */
+import java.math.*;
+import java.util.*;
+import java.lang.*;
+
 public class NonAbundantSums {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        
+    }
+    
+    public static int getSumOfAbundants(ArrayList<Integer> numbers)
+    {
+        return 1;
+    }
+    
+    public static boolean isAbundant(int number)
+    {
+        return true;
+    }
+    
+    public static ArrayList<Integer> getDivisorsOf(int number)
+    {
+        ArrayList<Integer> divisors = new ArrayList<>();
+        
+        
+        
+        return divisors;
     }
     
 }
