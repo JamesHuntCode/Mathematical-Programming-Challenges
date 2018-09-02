3ccfdab866a90a3cada9bb538c24f95d9b44aec7
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-09-02 23:25:02 +0100

    main program algorithm written - now working on smaller methods

--- Goldbatch-Conjecture/GoldbatchConjectureChallenge/src/goldbatchconjecturechallenge/GoldbatchConjectureChallenge.java
+++ Goldbatch-Conjecture/GoldbatchConjectureChallenge/src/goldbatchconjecturechallenge/GoldbatchConjectureChallenge.java
@@ -8,7 +8,16 @@ public class GoldbatchConjectureChallenge {
 
     public static void main(String[] args) {
         
+        int smallestValue = 0;
         
+        ArrayList<Integer> composites = getOddComposites(100);
+        
+        for (int i = 0; i < composites.size(); i++)
+        {
+            if (!followsSuit(composites.get(i))) smallestValue = composites.get(i);
+        }
+        
+        System.out.print("Smallest odd composite not to be the sum of a prime and twice a square is: " + smallestValue);
         
     }
     
@@ -22,7 +31,7 @@ public class GoldbatchConjectureChallenge {
     }
     
     
-    public static ArrayList<Integer> getComposites(int terms)
+    public static ArrayList<Integer> getOddComposites(int terms)
     {
         ArrayList<Integer> composites = new ArrayList<>();
         
