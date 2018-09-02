38b087381df5a611f4d0015975afc7465dbfe8f3
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-09-02 23:14:40 +0100

    skeleton methods laid out

--- Goldbatch-Conjecture/GoldbatchConjectureChallenge/src/goldbatchconjecturechallenge/GoldbatchConjectureChallenge.java
+++ Goldbatch-Conjecture/GoldbatchConjectureChallenge/src/goldbatchconjecturechallenge/GoldbatchConjectureChallenge.java
@@ -12,4 +12,28 @@ public class GoldbatchConjectureChallenge {
         
     }
     
+    public static ArrayList<Integer> getPrimes(int terms)
+    {
+        ArrayList<Integer> primes = new ArrayList<>();
+        
+        
+        
+        return primes;
+    }
+    
+    
+    public static ArrayList<Integer> getComposites(int terms)
+    {
+        ArrayList<Integer> composites = new ArrayList<>();
+        
+        
+        
+        return composites;
+    }
+    
+    public static boolean followsSuit(int number)
+    {
+        return true;
+    }
+    
 }
